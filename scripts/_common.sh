8abdb0ccda266bbeefacee304ad83c60eda55c18
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-11 18:30:58 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="supervisor"
+#REMOVEME? pkg_dependencies="supervisor"
 nodejs_version=12
 
 #=================================================
